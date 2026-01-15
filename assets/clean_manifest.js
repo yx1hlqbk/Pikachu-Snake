de9497bf115d33786303be7f69dc51539d5ee4e3
DESKTOP-B4SB3CO\user <yx1hlqbk@gmail.com>
2026-01-15 15:12:10 +0800

    perf: Localize all Pokemon assets for improved loading speed

--- assets/clean_manifest.js
+++ assets/clean_manifest.js
@@ -7,91 +7,90 @@ window.ASSET_MANIFEST = {
     // Starter Pokemon (Normal Food) - All 9 Generations
     starters: [
         // Generation 1
-        { id: 1, name: "妙蛙種子", src: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png' },
-        { id: 4, name: "小火龍", src: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png' },
-        { id: 7, name: "傑尼龜", src: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png' },
+        { id: 1, name: "妙蛙種子", src: 'assets/sprites/pokemon/1.png' },
+        { id: 4, name: "小火龍", src: 'assets/sprites/pokemon/4.png' },
+        { id: 7, name: "傑尼龜", src: 'assets/sprites/pokemon/7.png' },
 
         // Generation 2
-        { id: 152, name: "菊草葉", src: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/152.png' },
-        { id: 155, name: "火球鼠", src: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/155.png' },
-        { id: 158, name: "小鋸鱷", src: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/158.png' },
+        { id: 152, name: "菊草葉", src: 'assets/sprites/pokemon/152.png' },
+        { id: 155, name: "火球鼠", src: 'assets/sprites/pokemon/155.png' },
+        { id: 158, name: "小鋸鱷", src: 'assets/sprites/pokemon/158.png' },
 
         // Generation 3
-        { id: 252, name: "木守宮", src: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/252.png' },
-        { id: 255, name: "火稚雞", src: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/255.png' },
-        { id: 258, name: "水躍魚", src: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/258.png' },
+        { id: 252, name: "木守宮", src: 'assets/sprites/pokemon/252.png' },
+        { id: 255, name: "火稚雞", src: 'assets/sprites/pokemon/255.png' },
+        { id: 258, name: "水躍魚", src: 'assets/sprites/pokemon/258.png' },
 
         // Generation 4
-        { id: 387, name: "草苗龜", src: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/387.png' },
-        { id: 390, name: "小火焰猴", src: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/390.png' },
-        { id: 393, name: "波加曼", src: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/393.png' },
+        { id: 387, name: "草苗龜", src: 'assets/sprites/pokemon/387.png' },
+        { id: 390, name: "小火焰猴", src: 'assets/sprites/pokemon/390.png' },
+        { id: 393, name: "波加曼", src: 'assets/sprites/pokemon/393.png' },
 
         // Generation 5
-        { id: 495, name: "藤藤蛇", src: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/495.png' },
-        { id: 498, name: "暖暖豬", src: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/498.png' },
-        { id: 501, name: "水水獺", src: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/501.png' },
+        { id: 495, name: "藤藤蛇", src: 'assets/sprites/pokemon/495.png' },
+        { id: 498, name: "暖暖豬", src: 'assets/sprites/pokemon/498.png' },
+        { id: 501, name: "水水獺", src: 'assets/sprites/pokemon/501.png' },
 
         // Generation 6
-        { id: 650, name: "哈力栗", src: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/650.png' },
-        { id: 653, name: "火狐狸", src: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/653.png' },
-        { id: 656, name: "呱呱泡蛙", src: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/656.png' },
+        { id: 650, name: "哈力栗", src: 'assets/sprites/pokemon/650.png' },
+        { id: 653, name: "火狐狸", src: 'assets/sprites/pokemon/653.png' },
+        { id: 656, name: "呱呱泡蛙", src: 'assets/sprites/pokemon/656.png' },
 
         // Generation 7
-        { id: 722, name: "木木梟", src: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/722.png' },
-        { id: 725, name: "火斑喵", src: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/725.png' },
-        { id: 728, name: "球球海獅", src: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/728.png' },
+        { id: 722, name: "木木梟", src: 'assets/sprites/pokemon/722.png' },
+        { id: 725, name: "火斑喵", src: 'assets/sprites/pokemon/725.png' },
+        { id: 728, name: "球球海獅", src: 'assets/sprites/pokemon/728.png' },
 
         // Generation 8
-        { id: 810, name: "敲音猴", src: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/810.png' },
-        { id: 813, name: "炎兔兒", src: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/813.png' },
-        { id: 816, name: "淚眼蜥", src: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/816.png' },
+        { id: 810, name: "敲音猴", src: 'assets/sprites/pokemon/810.png' },
+        { id: 813, name: "炎兔兒", src: 'assets/sprites/pokemon/813.png' },
+        { id: 816, name: "淚眼蜥", src: 'assets/sprites/pokemon/816.png' },
 
         // Generation 9
-        { id: 906, name: "新葉喵", src: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/906.png' },
-        { id: 909, name: "呆火鱷", src: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/909.png' },
-        { id: 912, name: "潤水鴨", src: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/912.png' }
+        { id: 906, name: "新葉喵", src: 'assets/sprites/pokemon/906.png' },
+        { id: 909, name: "呆火鱷", src: 'assets/sprites/pokemon/909.png' },
+        { id: 912, name: "潤水鴨", src: 'assets/sprites/pokemon/912.png' }
     ],
 
     // Legendary Pokemon (Special Food)
     legendaries: [
         // Generation 1
-        { id: 150, name: "超夢", src: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/150.png' },
-        { id: 151, name: "夢幻", src: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/151.png' },
+        { id: 150, name: "超夢", src: 'assets/sprites/pokemon/150.png' },
+        { id: 151, name: "夢幻", src: 'assets/sprites/pokemon/151.png' },
 
         // Generation 2
-        { id: 249, name: "洛奇亞", src: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/249.png' },
-        { id: 249, name: "洛奇亞", src: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/249.png' },
-        { id: 250, name: "鳳王", src: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/250.png' },
+        { id: 249, name: "洛奇亞", src: 'assets/sprites/pokemon/249.png' },
+        { id: 250, name: "鳳王", src: 'assets/sprites/pokemon/250.png' },
 
         // Generation 3
-        { id: 382, name: "蓋歐卡", src: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/382.png' },
-        { id: 383, name: "固拉多", src: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/383.png' },
-        { id: 384, name: "烈空坐", src: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/384.png' },
+        { id: 382, name: "蓋歐卡", src: 'assets/sprites/pokemon/382.png' },
+        { id: 383, name: "固拉多", src: 'assets/sprites/pokemon/383.png' },
+        { id: 384, name: "烈空坐", src: 'assets/sprites/pokemon/384.png' },
 
         // Generation 4
-        { id: 483, name: "帝牙盧卡", src: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/483.png' },
-        { id: 484, name: "帕路奇亞", src: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/484.png' },
-        { id: 487, name: "騎拉帝納", src: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/487.png' },
+        { id: 483, name: "帝牙盧卡", src: 'assets/sprites/pokemon/483.png' },
+        { id: 484, name: "帕路奇亞", src: 'assets/sprites/pokemon/484.png' },
+        { id: 487, name: "騎拉帝納", src: 'assets/sprites/pokemon/487.png' },
 
         // Generation 5
-        { id: 643, name: "雷希拉姆", src: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/643.png' },
-        { id: 644, name: "捷克羅姆", src: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/644.png' },
-        { id: 646, name: "酋雷姆", src: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/646.png' },
+        { id: 643, name: "雷希拉姆", src: 'assets/sprites/pokemon/643.png' },
+        { id: 644, name: "捷克羅姆", src: 'assets/sprites/pokemon/644.png' },
+        { id: 646, name: "酋雷姆", src: 'assets/sprites/pokemon/646.png' },
 
         // Generation 6
-        { id: 716, name: "哲爾尼亞斯", src: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/716.png' },
-        { id: 717, name: "伊裴爾塔爾", src: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/717.png' },
+        { id: 716, name: "哲爾尼亞斯", src: 'assets/sprites/pokemon/716.png' },
+        { id: 717, name: "伊裴爾塔爾", src: 'assets/sprites/pokemon/717.png' },
 
         // Generation 7
-        { id: 791, name: "索爾迦雷歐", src: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/791.png' },
-        { id: 792, name: "露奈雅拉", src: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/792.png' },
+        { id: 791, name: "索爾迦雷歐", src: 'assets/sprites/pokemon/791.png' },
+        { id: 792, name: "露奈雅拉", src: 'assets/sprites/pokemon/792.png' },
 
         // Generation 8
-        { id: 888, name: "蒼響", src: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/888.png' },
-        { id: 889, name: "藏瑪然特", src: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/889.png' },
+        { id: 888, name: "蒼響", src: 'assets/sprites/pokemon/888.png' },
+        { id: 889, name: "藏瑪然特", src: 'assets/sprites/pokemon/889.png' },
 
         // Generation 9
-        { id: 1007, name: "故勒頓", src: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1007.png' },
-        { id: 1008, name: "密勒頓", src: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1008.png' }
+        { id: 1007, name: "故勒頓", src: 'assets/sprites/pokemon/1007.png' },
+        { id: 1008, name: "密勒頓", src: 'assets/sprites/pokemon/1008.png' }
     ]
 };
